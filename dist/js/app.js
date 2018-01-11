3d9533eb605ad85acbc775615a1e63c1424ed5b2
Thiago Galvani <thiagopaiva99@gmail.com>
2018-01-11 00:28:08 -0200

    Animals and forests hidding and showing

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,32 @@
-$('.tab-menu a').on('click', function(e) {
+$('.animals .tab-menu a').first().addClass('active')
+$('.animals .item').first().addClass('active')
+
+$('.animals .tab-menu a').on('click', function(e) {
     // stop default execution of the link
     e.preventDefault()
 
     // remove active classes from itens
-    $('.tab-menu a, .item').removeClass('active')
+    $('.animals .tab-menu a, .animals .item').removeClass('active')
+
+    // get the item id
+    const itemId = $(this).attr('href')
+
+    // add active class to button clicked
+    $(this).addClass('active')
+
+    // add active class to the item content
+    $(itemId).addClass('active')
+})
+
+$('.florestas .tab-menu a').first().addClass('active')
+$('.florestas .item').first().addClass('active')
+
+$('.florestas .tab-menu a').on('click', function(e) {
+    // stop default execution of the link
+    e.preventDefault()
+
+    // remove active classes from itens
+    $('.florestas .tab-menu a, .florestas .item').removeClass('active')
 
     // get the item id
     const itemId = $(this).attr('href')
